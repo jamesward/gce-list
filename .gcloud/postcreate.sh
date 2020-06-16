a6099dc6cb50e9c1806f7f7721b4fa28f863c819
James Ward <james@jamesward.com>
2020-06-16 10:47:38 -0600

    quieter

--- .gcloud/postcreate.sh
+++ .gcloud/postcreate.sh
@@ -14,7 +14,7 @@ if [ $? -ne 0 ]; then
   echo "creating service account: $sa"
   gcloud iam service-accounts create gce-list \
     --display-name="$service" \
-    --project=$project
+    --project=$project &> /dev/null
 
   echo "gonna wait 30 seconds for iam consistency"
   sleep 30
@@ -23,15 +23,15 @@ fi
 echo "allowing $sa to list GCE instances"
 gcloud projects add-iam-policy-binding $project \
   --member=serviceAccount:$sa \
-  --role=roles/compute.viewer
+  --role=roles/compute.viewer &> /dev/null
 
 echo "allowing $sa to be a serviceAccountUser"
 gcloud projects add-iam-policy-binding $project \
   --member=serviceAccount:$sa \
-  --role=roles/iam.serviceAccountUser
+  --role=roles/iam.serviceAccountUser &> /dev/null
 
 echo "updating $service to use the service account $sa"
 gcloud run services update $service \
   --platform=managed --project=$project --region=$region \
-  --service-account=$sa
+  --service-account=$sa &> /dev/null
 
