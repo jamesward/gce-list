4f495a0f5d0dbdc0ac059786bd371a5a4dc320fc
James Ward <james@jamesward.com>
2020-06-16 07:10:59 -0600

    add project query

--- app.sh
+++ app.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-if result=$(gcloud compute instances list --format='value(name)' 2>&1); then
+readonly projectid=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/project/project-id)
+
+if result=$(gcloud compute instances list --project=$projectid --format='value(name)' 2>&1); then
   length=$(echo $result | wc -c)
   echo -e "HTTP/1.1 200 OK\nContent-Length: $length\n\n$result"
 else
