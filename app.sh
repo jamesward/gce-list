d9dfcfd87559257492971f23f5d6a7df7627a4f7
James Ward <james@jamesward.com>
2020-06-16 07:15:43 -0600

    ummmm

--- app.sh
+++ app.sh
@@ -2,10 +2,12 @@
 
 readonly projectid=$(curl -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/project/project-id)
 
-if result=$(gcloud compute instances list --project=$projectid --format='value(name)' 2>&1); then
-  length=$(echo $result | wc -c)
-  echo -e "HTTP/1.1 200 OK\nContent-Length: $length\n\n$result"
-else
-  length=$(echo $result | wc -c)
-  echo -ne "HTTP/1.1 500 OK\nContent-Length: $length\n\n$result"
-fi
+echo -e "HTTP/1.1 200 OK\n\nhello, world"
+
+#if result=$(gcloud compute instances list --project=$projectid --format='value(name)' 2>&1); then
+#  length=$(echo $result | wc -c)
+#  echo -e "HTTP/1.1 200 OK\nContent-Length: $length\n\n$result"
+#else
+#  length=$(echo $result | wc -c)
+#  echo -ne "HTTP/1.1 500 OK\nContent-Length: $length\n\n$result"
+#fi
