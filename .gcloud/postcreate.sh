a9c1b2f30eaf3b6a71b3b5aa06abd7606e531d7d
James Ward <james@jamesward.com>
2020-06-16 10:29:37 -0600

    try this

---
+++ .gcloud/postcreate.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+declare project=$GOOGLE_CLOUD_PROJECT
+declare service=$K_SERVICE
+declare region=$GOOGLE_CLOUD_REGION
+
+declare sa=gce-list@$project.iam.gserviceaccount.com
+
+gcloud iam service-accounts describe $sa --project $project &> /dev/null
+
+if [ $? -ne 0 ]; then
+  echo "creating service account: $sa"
+  gcloud iam service-accounts create gce-list \
+    --display-name="$service" \
+    --project=$project
+
+  echo "gonna wait 30 seconds for iam consistency"
+  sleep 30
+fi
+
+echo "allowing $sa to list GCE instances"
+gcloud projects add-iam-policy-binding $project \
+  --member=serviceAccount:$sa \
+  --role=roles/compute.viewer &> /dev/null
+
+echo "allowing $sa to be a serviceAccountUser"
+gcloud projects add-iam-policy-binding $project \
+  --member=serviceAccount:$sa \
+  --role=roles/iam.serviceAccountUser &> /dev/null
+
+echo "updating $service to use the service account $sa"
+gcloud run services update $service \
+  --platform=managed --project=$project --region=$region \
+  --service-account=$sa &> /dev/null
+
--- app.sh
+++ app.sh
@@ -3,4 +3,4 @@
 readonly projectid=$(curl -s -H "Metadata-Flavor: Google" http://metadata.google.internal/computeMetadata/v1/project/project-id)
 
 readonly result=$(gcloud compute instances list --project=$projectid --format='value(name)' 2>&1)
-echo -e "HTTP/1.1 200 OK\n\n$result"
+echo -e "HTTP/1.0 200 OK\n\ninstances:\n$result"
